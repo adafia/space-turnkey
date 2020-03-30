3cad109950f72b1f0f672c3bbd6bffe34f5e8795
adafia <adafia.samuel@gmail.com>
2020-03-30 17:40:41 +0200

    fix build

--- config/testKeys.js
+++ config/testKeys.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   DATABASE_URL: process.env.MONGODB_CONNECTION_STRING,
-  PRIVATE_KEY: process.env.PRIVATE_KEY,
+  PRIVATE_KEY: process.env.PRIVATE_KEY_TEST,
 };
